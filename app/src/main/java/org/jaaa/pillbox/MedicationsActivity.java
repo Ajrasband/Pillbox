df2685d9de17edab850736855830f119b256b2e3
TheArduinist <alek.spackman@gmail.com>
2017-04-21 13:57:28 -0600

    implemented medication activity

--- app/src/main/java/org/jaaa/pillbox/MedicationsActivity.java
+++ app/src/main/java/org/jaaa/pillbox/MedicationsActivity.java
@@ -2,6 +2,7 @@ package org.jaaa.pillbox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 
 public class MedicationsActivity extends Activity
 {
@@ -9,6 +10,9 @@ public class MedicationsActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_medication);
+        ExpandableListView expandableList = (ExpandableListView)findViewById(R.id.list_view);
+        MedicationExpandableListAdapter adapter = new MedicationExpandableListAdapter(this, null);
+        expandableList.setAdapter(adapter);
     }
 }
