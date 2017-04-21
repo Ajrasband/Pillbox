fe5130cc58f8957807fc0898e1ec6699f1517238
andrewtakeshi <icehockeyandrew2@gmail.com>
2017-04-21 13:24:02 -0600

    Merge branch 'julie' into alek
    # Conflicts:
    #	app/src/main/java/org/jaaa/pillbox/HomeActivity.java

--- app/src/main/java/org/jaaa/pillbox/CalendarActivity.java
+++ app/src/main/java/org/jaaa/pillbox/CalendarActivity.java
@@ -1,10 +1,17 @@
 package org.jaaa.pillbox;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by iceho on 4/21/2017.
  */
 
 public class CalendarActivity extends Activity {
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.id.activity_calendar);
+
+    }
+
 }
